89ddc1fcf45b5bd7221e08d620ecda2045dd5db4
nalemub <natnaelalemu5@gmail.com>
2023-04-03 12:53:39 -0700

    commit msg

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,3 @@
-#include"main.h"
-
-
 /**a fuction that copies a specified number of bytes    *from one memory location and return it to another    *location
   *
   *@dest is the destination location
@@ -10,9 +7,14 @@
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  for(int i=0; i<n;i++)
+  unsigned int i;
+  for (i=0; i<n;i++)
     {
       dest[i]=src[i];
     }
   return (dest);
 }
+~                                                                                                                                   
+~                                                                                                                                   
+~                                                                                                                                   
+~                     
