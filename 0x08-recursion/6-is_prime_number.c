34f8f68841e83b1d1e37374adee9188606a59736
nalemub <natnaelalemu5@gmail.com>
2023-04-05 13:38:43 -0700

    msg

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,48 +1,50 @@
 /**
  * the_true_function - Helper function to check if a number is prime
+ *
  * @n: The number to check
  * @factor: The current factor being checked
  *
  * Return: 1 if the number is prime, 0 otherwise
  */
-
 int the_true_function(int n, int factor)
 {
-if (n % factor == 0 && factor > 1)
-{
-  return (0);
-}
-else if (factor == 1)
-{
-  return (1);
-}
+	if (n % factor == 0 && factor > 1)
+	{
+		return (0);
+	}
+	else if (factor == 1)
+	{
+		return (1);
+	}
 
-  factor--;
-  return (the_true_function(n, factor));
+	factor--;
+	return (the_true_function(n, factor));
 }
 
 /**
  * is_prime_number - Determines if a number is prime
+ *
  * @n: The number to check
- * @m: The factor that checks for the number
  *
  * Return: 1 if the number is prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
-int m;
-int answer;
-if ((n*n) == 1)
-{
-  return 0;
-}
-  
-else if (n < 0)
-{
-  n = n*-1;
+	int m;
+	int answer;
+
+	if ((n * n) == 1)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		n = n * -1;
+	}
+
+	m = n - 1;
+	answer = the_true_function(n, m);
+
+	return (answer);
 }
 
-m = n-1;
-answer = the_true_function(n, m);
-return (answer);
-}
