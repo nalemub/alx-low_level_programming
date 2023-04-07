446336caa7d06f3b9039000060f535252080871c
nalemub <125489359+nalemub@users.noreply.github.com>
2023-04-07 23:55:52 +0300

    s

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: of characters to match
+ *
+ * Return: the number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
