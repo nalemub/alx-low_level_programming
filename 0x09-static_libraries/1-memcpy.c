ba106208e333db3445c3cb39a89ee50d1bc3d0b9
nalemub <125489359+nalemub@users.noreply.github.com>
2023-04-07 23:52:21 +0300

    s

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,12 @@
+/**
+ * _memcpy - copies memory area
+ * @dest: destination memory
+ * @src: source memory
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the destination memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
