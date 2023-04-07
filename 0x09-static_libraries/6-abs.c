9514bd58f273b96a895b4d0c9c8edc0619bd5e4a
nalemub <125489359+nalemub@users.noreply.github.com>
2023-04-07 23:27:03 +0300

    s

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of an integer
+ *
+ * @n: the integer to compute absolute value for
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	
+}
