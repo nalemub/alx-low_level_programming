2f94e54f2555951b2519f70e22b1a6b09ce7d569
nalemub <125489359+nalemub@users.noreply.github.com>
2023-04-07 23:50:59 +0300

    s

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,12 @@
+/**
+ * _memset - fills memory with a constant byte
+ * @s: area to fill
+ * @b: the byte to fill the memory with
+ * @n: number of bytes to fill
+ *
+ * Return: memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
