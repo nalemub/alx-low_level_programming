75daf8e4d310961a1cf873b61b799163ccec5a3f
nalemub <125489359+nalemub@users.noreply.github.com>
2023-04-07 23:48:04 +0300

    s

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ *
+ * Return: pointer to the destination buffer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
