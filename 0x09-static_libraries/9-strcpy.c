ae71c99adc94097e645a396d8adf3fcbf0eee497
nalemub <125489359+nalemub@users.noreply.github.com>
2023-04-07 23:35:59 +0300

    c

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src to dest
+ *
+ * @dest: destination
+ * @src: source 
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	
+}
