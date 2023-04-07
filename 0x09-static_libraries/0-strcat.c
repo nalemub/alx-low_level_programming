28198791bedc68639cc37f7a36827b9aa3cbb71c
nalemub <125489359+nalemub@users.noreply.github.com>
2023-04-07 23:38:26 +0300

    s

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: second string
+ * @src: first string
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	
+}
