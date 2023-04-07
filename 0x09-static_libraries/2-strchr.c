39fb8e84f398526d7c66ce239716abae13e1813c
nalemub <125489359+nalemub@users.noreply.github.com>
2023-04-07 23:53:47 +0300

    s

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,11 @@
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to search
+ * @c: the character to locate
+ *
+ * Return: pointer to the first occurrence ofa character
+ */
+char *_strchr(char *s, char c)
+{
+
+}
