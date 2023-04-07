83d3bce7b59b7f2a5bb640b5bae83fedb32ae859
nalemub <125489359+nalemub@users.noreply.github.com>
2023-04-07 23:58:51 +0300

    s

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,13 @@
+$include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search
+ * @needle: substring to find
+ *
+ * Return: pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
