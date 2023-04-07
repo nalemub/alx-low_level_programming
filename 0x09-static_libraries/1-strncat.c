16945e2c0d72fa1fe684c3cc22661f427bb2f063
nalemub <125489359+nalemub@users.noreply.github.com>
2023-04-07 23:39:55 +0300

    s

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings up to n bytes
+ *
+ * @dest: the destination string
+ * @src: string
+ * @n: number of bytes
+ *
+ * Return: the pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	
+}
