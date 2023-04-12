7a6a04f0891634a720ba1e542f1c402d0e54d5c7
nalemub <natnaelalemu5@gmail.com>
2023-04-12 13:47:52 -0700

    nas

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,5 @@
 #include <stdlib.h> 
-
+#include "main.h"
 /**
  * a function that allocates a dynamic memory
  * 
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,5 +1,5 @@
 #include <stdlib.h> 
-
+#include "main.h"
 /**
  * a function that takes min and max, and lists all numbers between
  * 
