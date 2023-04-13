2f58823310fea1bef83bf58a6d70a009c5355b16
nalemub <Natnaelalemu5@gmail.com>
2023-04-13 21:52:54 +0300

    commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,11 +2,11 @@
 #include "main.h"
 /**
  * a function that takes min and max, and lists all numbers between
- * 
+ *
  * @newmem: some pointer
- * 
+ *
  * @rangenum: some variable
- * 
+ *
  * return: void
 */
 
