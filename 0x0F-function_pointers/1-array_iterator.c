92008bbbb3dbe3a4de6a10ab15b18a2713244575
nalemub <Natnaelalemu5@gmail.com>
2023-04-19 22:59:26 +0300

    fixes

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,9 +2,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int i;
-if (array != NULL && size != NULL && action != NULL)
-for (int i = 0; i < size; i++ )
+long unsigned int i;
+if (array != NULL && size != 0 && action != NULL)
+for (i = 0; i < size; i++ )
 {
     action(array[i]);
 }
